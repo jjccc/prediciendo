942deeb1c4fbe6f40c28e15a19c2e25058ea05f7
jjccc <jjcanocortes@outlook.com>
2014-11-11 00:21:53 +0100

    Cambio de la carpeta de uploads

--- app/models/author.rb
+++ app/models/author.rb
@@ -10,18 +10,18 @@ class Author < ActiveRecord::Base
     (self.successes + self.mistakes) == 0 ? 0.0 : (self.successes.to_f / (self.mistakes + self.successes).to_f * 100)
   end
   
-  def has_image?
-    !self.image.nil? && File.exists?(Rails.root.join("public", "images", self.image))
+  def has_image?    
+    !self.image.nil? && File.exists?(path)
   end
   
   def image_path
-    "/images/#{self.image}"
+    Rails.env.production? ? "/system/uploads/#{self.image}" : "/images/#{self.image}"
   end
   
   def process_image        
     unless self.file.blank?
       self.image = "#{self.name.parameterize}.#{self.file.original_filename.split(".")[1]}"      
-      File.open(Rails.root.join("public", "images", self.image), "wb") do |f|
+      File.open(path, "wb") do |f|
         f.write(self.file.read)
       end
     end    
@@ -33,6 +33,12 @@ class Author < ActiveRecord::Base
     self.successes = current_predictions.select{ |p| p.is_pending == false && p.is_success == true }.count
     self.mistakes = current_predictions.select{ |p| p.is_pending == false && p.is_success == false }.count
     self.save
+  end
+  
+  private 
+  
+  def path
+    Rails.env.production? ? Rails.root.join("public", "system", "uploads", self.image) : Rails.root.join("public", "images", self.image)
   end
   
 end
